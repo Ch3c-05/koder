7ac2fe6dd0c25b150e93c597e38e8a0ed94cec93
Ch3c-05 <ungureanuandrei187@gmail.com>
2025-08-31 23:25:52 +0200

    final code

--- Koder 3.g/nim.py
+++ Koder 3.g/nim.py
@@ -1,27 +1,48 @@
+import random
+
+bricks_emoji = "🧱"
 bricks_alt = 21
 bricks = 0
 player = 0
 
 
+def show_bricks():
+    
+    print("Brikkerne tilbage:", bricks_emoji * bricks_alt)
+
+
 def computer():
     
     global bricks, bricks_alt
-    print("Der er", bricks_alt, "brikker tilbage.")
-    if bricks_alt % 4 == 3:
-        bricks = 2
-    elif bricks_alt % 4 == 2:
-        bricks = 1
-    elif bricks_alt % 4 == 0:
-        bricks = 3
-    else:
-        bricks = 1 
-    print("CPU takes some", bricks)
-    bricks_alt -= bricks
-    print("Der er", bricks_alt, "brikker tilbage.")
+    show_bricks()
+
+    #Den stykke kode er faktisk algoritmen som man bruger for at vinder spillet.
+
+    #print("Der er", bricks_alt, "brikker tilbage.")
+    #if bricks_alt % 4 == 3:
+     #   bricks = 2
+    #elif bricks_alt % 4 == 2:
+    #    bricks = 1
+    #elif bricks_alt % 4 == 0:
+    #    bricks = 3
+    #else:
+     #   bricks = 1 
+    #print("CPU takes some", bricks)
+    #bricks_alt -= bricks
+    #print("Der er", bricks_alt, "brikker tilbage.")
     
+    print("Der er nu", bricks_alt, "brikker tilbage.")
+    bricks = random.randint(1, min(3, bricks_alt))
+    print("CPU tager", bricks, "brikker")
+    bricks_alt -= bricks
+
+
+
 def p1():
         
     global player, bricks_alt
+    show_bricks()
+    print("Der er nu", bricks_alt, "brikker tilbage.")
     
     while True:
         try:
@@ -52,6 +73,7 @@ def game_loop():
             print("CPU har tabt!")
             break
         computer()
+
 
 
 game_loop()
